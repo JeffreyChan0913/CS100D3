3503846b64a9f42c7a0427b27f75fcd7dd008908
jeffrey <jeffrey913@outlook.com>
2023-08-17 14:46:05 -0700

    update CMakeLists.txt without old cpp files and update the sh files to linux and mac compatible

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 BLUE='\033[1;34m'
 NC='\033[0m'
-echo "${BLUE}Clean up${NC}"
+if [ "$(uname)" = "Darwin" ]; then
+	echo "${BLUE}Clean up${NC}"
+else
+	echo -e "${BLUE}Clean up${NC}"
+fi;
 rm -rf build
--- script/init.sh
+++ script/init.sh
@@ -2,12 +2,24 @@
 GRN='\033[32;1m'
 BLUE='\033[34;1m'
 NC="\033[0m"
-echo "${GRN}Initializing ${NC}the ${BLUE}script${NC}"
-echo "${GRN}Creating ${BLUE}build directory${NC}$"
-mkdir build
-echo  "${GRN}Switching to ${BLUE}build folder${NC}"
-cd build
-echo  "${GRN}cmake .. -- basic log${NC}"
-cmake ..
-echo  "${GRN}make -- basic log${NC}"
-make
+if [ "$(uname)" = "Darwin" ]; then 
+	echo "${GRN}Initializing ${NC}the ${BLUE}script${NC}"
+	echo "${GRN}Creating ${BLUE}build directory${NC}$"
+	mkdir build
+	echo  "${GRN}Switching to ${BLUE}build folder${NC}"
+	cd build
+	echo  "${GRN}cmake .. -- basic log${NC}"
+	cmake ..
+	echo  "${GRN}make -- basic log${NC}"
+	make
+else
+	echo -e "${GRN}Initializing ${NC}the ${BLUE}script${NC}"
+	echo -e "${GRN}Creating ${BLUE}build directory${NC}$"
+	mkdir build
+	echo -e "${GRN}Switching to ${BLUE}build folder${NC}"
+	cd build
+	echo -e "${GRN}cmake .. -- basic log${NC}"
+	cmake ..
+	echo -e "${GRN}make -- basic log${NC}"
+	make
+fi;
