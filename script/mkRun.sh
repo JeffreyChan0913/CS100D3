5c2d102e5667ea3348442f706ec1b9a88b91b558
jeffrey <jeffrey913@outlook.com>
2023-08-17 14:33:46 -0700

    modify the mkRun.sh to linux and mac compatible

--- script/mkRun.sh
+++ script/mkRun.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
 GRN='\033[32;1m'
 BLUE='\033[34;1m'
 NC="\033[0m"
-echo "${GRN}Compile ${BLUE}run ${NC}and ${BLUE}removebuild${NC}"
-gcc script/start.c -o run && gcc script/removebuild.c -o removebuild
-echo "${GRN}Copy ${NC}to ${BLUE}..${NC}"
-cp run removebuild ..
+
+if [ "$(uname)" = "Darwin" ]; then
+	echo "${GRN}Compile ${BLUE}executable files: run ${NC}and ${BLUE}removebuild${NC}"
+	gcc script/start.c -o run && gcc script/removebuild.c -o removebuild
+	echo "${GRN}Copy executables${NC} to ${BLUE}location -> ..${NC}"
+	cp run removebuild ..
+else
+	echo -e "${GRN}Compile ${BLUE}executable files: run ${NC}and ${BLUE}removebuild${NC}"
+	gcc script/start.c -o run && gcc script/removebuild.c -o removebuild
+	echo -e "${GRN}Copy executables${NC} to ${BLUE}location -> ..${NC}"
+	cp run removebuild ..
+fi;
